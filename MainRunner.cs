dea7b07307c9b014c75add627dc18e2b6112b36e
shatram193 <shatram193@potsdam.edu>
2013-04-28 20:47:32 -0400

    Server added, networking apparently functional.
    There is now a multithreaded TCP server functioning in this directory.
    Beyond that, we'll see.

--- MainRunner.cs
+++ MainRunner.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
 
 namespace Arcana
 {
@@ -11,6 +15,20 @@ namespace Arcana
         {
             using (ArcanaMain game = new ArcanaMain())
             {
+                Server server = new Server();
+                TcpClient client = new TcpClient();
+
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000);
+
+                client.Connect(serverEndPoint);
+
+                NetworkStream clientStream = client.GetStream();
+
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                byte[] buffer = encoder.GetBytes("Hello Server!");
+
+                clientStream.Write(buffer, 0, buffer.Length);
+                clientStream.Flush();
                 game.Run();
             }
         }
